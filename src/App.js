d06e978f8fc40671133bc939401e54106a485335
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-06-20 18:25:56 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
-import ProfileEdit from './ProfileEdit';
+import React, { useState } from "react";
+import "../src/App.css";
+import ProfileEdit from "./ProfileEdit";
 
 function App() {
-  return <ProfileEdit />
-  
+  const [userId, setUserID] = useState(1);
+
+  const userIds = [1, 2, 3, 4];
+
+  return (
+    <div className="App">
+      {userIds.map((id) => (
+        <button key={id} onClick={() => setUserID(id)}>
+          User ID {id}
+        </button>
+      ))}
+      <h2>User ID {userId}</h2>
+      <ProfileEdit userID={userId} />
+    </div>
+  );
 }
 
 export default App;
