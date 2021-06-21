d6dcd365a23d5a04aca3b42b8209112d7cc5a5b1
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-06-20 19:29:24 -0600

    practice with effect hook

--- src/ProfileEdit.js
+++ src/ProfileEdit.js
@@ -1,20 +1,91 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 
-function ProfileEdit({ userID }) {  // Added `userID` as a prop
+function ProfileEdit({ userID }) {
   const [user, setUser] = useState({});
 
   useEffect(() => {
-    async function loadUser() {
-      const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userID}`);
-      const userFromAPI = await response.json();
-      setUser(userFromAPI);
+    setUser({});
+    const abortController = new AbortController()
+
+    async function loadUsers() {
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/users/${userID}`,
+          { signal: abortController.signal }
+        );
+        const userFromAPI = await response.json();
+        setUser(userFromAPI);
+      } catch (error) {
+        if (error.name === 'AbortError') {
+          console.log('Aborted', userID);
+        } else {
+          throw error;
+        }
+      }
+    }  
+    loadUsers();
+
+    return () => {
+      console.log('cleanup', userID);
+      abortController.abort();
+    };
+  }, [userID]); // Passing [] so that it only runs the effect once
+
+  useEffect(() => {
+    if (user.name) {
+      document.title = `${user.name} : Edit Profile`;
+    } else {
+      document.title = "Edit Profile";
     }
+  }, [user]); // Rerun this effect when the user changes
 
-    loadUser();
-  }, [userID]);  // <-- Added dependency on `userID`
+  const changeHandler = (event) => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
 
-  ... // Omitted for brevity
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${user.id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify(user),
+      }
+    );
+    const savedData = await response.json();
+    console.log("Saved user!", savedData);
+  };
+
+  if (user.id) {
+    return (
+      <form name="profileEdit" onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="username">User Name:</label>
+          <input
+            id="username"
+            name="username"
+            type="text"
+            required={true}
+            value={user.username}
+            onChange={changeHandler}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            required={true}
+            value={user.email}
+            onChange={changeHandler}
+          />
+        </div>
+        <button type="submit">Save</button>
+      </form>
+    );
+  }
+  return "Loading...";
 }
 
-
-export default ProfileEdit;
+export default ProfileEdit
